0d62c8ccf23dc237e4eddd0f44cd0b21e457ecdb
Jazz <jazzygermosen@gmail.com>
2025-10-02 20:16:46 -0400

    first commit

---
+++ src/main/java/com/pluralsight/FutureValue.java
@@ -0,0 +1,26 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class FutureValue {
+
+    public static Scanner valueFuture = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        // calculating the compound interest on a one time deposit
+        // the user is going to need to input the deposit, interest rate and number of years
+        // the future value and the total interest earned would be displayed
+        // the first step would be to set up a scanner on the outside of the main method
+        System.out.print("Deposit: ");
+        int deposit = valueFuture.nextInt();
+        System.out.println("Air: ");
+        double air = valueFuture.nextDouble();
+        System.out.println("Years: ");
+        int totalYears = valueFuture.nextInt();
+        int daysInYear = 365;
+        int totalDays = daysInYear*totalYears;
+        double futureValue = deposit* Math.pow((1+(air/daysInYear)), totalDays );
+        double interestEarned = futureValue - deposit;
+
+    }
+}
